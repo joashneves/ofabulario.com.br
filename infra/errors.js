396a34a3246ebc903c3c62a9a835c74e0ccaa5da
joashneves <joashneves@gmail.com>
2025-03-17 19:57:17 -0300

    feat: tests de users e verificação de duplicação

--- infra/errors.js
+++ infra/errors.js
@@ -56,3 +56,21 @@ export class MethodNotAllowedError extends Error {
     };
   }
 }
+
+export class ValidationError extends Error{
+  constructor({message, action}){
+    super(message || "Erro de validação ocorreu.");
+    this.name = "ValidationError";
+    this.action = action || "Verifique os dados enviados.";
+    this.statusCode = 409;
+  }
+
+  toJSON(){
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
