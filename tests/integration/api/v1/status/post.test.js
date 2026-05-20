fad9555cb18bb409eacda9cf55c2a54d94c2ee20
joashneves <joashneves@gmail.com>
2026-05-20 11:57:47 -0300

    feat: vakudate `user`, `feature` and `resource` in `authorization`  model

--- tests/integration/api/v1/status/post.test.js
+++ tests/integration/api/v1/status/post.test.js
@@ -1,17 +1,27 @@
 import orchestrator from "tests/orchestrator.js";
+import webserver from "infra/webserver.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  await orchestrator.clearDatabase();
 });
 
-describe("POST to /api/v1/status", () => {
+describe("POST /api/v1/status", () => {
   describe("Anonymous user", () => {
-    test("retrieving current system status", async () => {
-      const response = await fetch("http://localhost:3000/api/v1/status", {
+    test("Retrieving current system status", async () => {
+      const response = await fetch(`${webserver.origin}/api/v1/status`, {
         method: "POST",
       });
       expect(response.status).toBe(405);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "MethodNotAllowedError",
+        message: "Método não permitido para este endpoint.",
+        action:
+          "Verifique se o método HTTP enviado é válido para este endpoint.",
+        status_code: 405,
+      });
     });
   });
 });
